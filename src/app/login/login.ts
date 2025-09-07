2b7341f97e0509603ba4d0f00053bcc1a7a3f796
Sayeed Mohammad <sayeedmd05@gmail.com>
2025-09-07 00:17:32 -0500

    fix

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -5,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
